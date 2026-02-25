86992569ed7e0a699a29708afe4a5d5e7594e2c9
Gayatri Reddy <gayatrireddywgl@gmail.com>
2026-02-25 10:57:39 +0530

    Searching feature update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from expenses import Expense
 from datetime import datetime
 import csv
@@ -8,7 +9,11 @@ FILE_NAME = "expenses.csv"
 expenses = []
 expense_counter = 1
 
+
+# ================= LOAD EXPENSES =================
 def load_expenses():
+    global expenses, expense_counter
+
     expenses = []
 
     try:
@@ -18,38 +23,45 @@ def load_expenses():
             for row in reader:
                 expense = Expense(
                     int(row["id"]),
-                    datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S"),
+                    datetime.strptime(row["date"], "%Y-%m-%d"),
                     row["category"],
                     float(row["amount"]),
                     row["description"]
                 )
                 expenses.append(expense)
-    
+
+        expense_counter = len(expenses) + 1
+
     except FileNotFoundError:
         pass
 
-    return expenses
 
+# ================= SAVE EXPENSES =================
 def save_expenses():
     with open(FILE_NAME, mode="w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["ID", "Date", "Category", "Amount", "Description"])
+
+        # Always write correct lowercase headers
+        writer.writerow(["id", "date", "category", "amount", "description"])
 
         for expense in expenses:
             writer.writerow([
                 expense.expense_id,
-                expense.date,
+                expense.date.strftime("%Y-%m-%d"),  # save clean date
                 expense.category,
                 expense.amount,
                 expense.description
             ])
 
+
+# ================= VALIDATIONS =================
 def validate_date(date_text):
     try:
         datetime.strptime(date_text, "%Y-%m-%d")
         return True
     except ValueError:
         return False
+
 
 def validate_amount(amount_text):
     try:
@@ -60,14 +72,16 @@ def validate_amount(amount_text):
     except ValueError:
         return None
 
+
+# ================= ADD EXPENSE =================
 def add_expense():
     global expense_counter
-    
+
     print("\nAdd New Expense")
     print("-" * 20)
 
-    date = input("Enter date (YYYY-MM-DD): ")
-    if not validate_date(date):
+    date_input = input("Enter date (YYYY-MM-DD): ")
+    if not validate_date(date_input):
         print("Invalid date format!")
         return
 
@@ -86,7 +100,7 @@ def add_expense():
 
     expense = Expense(
         expense_id=expense_counter,
-        date=date,
+        date=datetime.strptime(date_input, "%Y-%m-%d"),
         category=category,
         amount=amount,
         description=description
@@ -96,38 +110,43 @@ def add_expense():
     expense_counter += 1
 
     save_expenses()
-    print("Expenses saved successfully!")
+    print("Expense added successfully!")
 
+
+# ================= VIEW EXPENSES =================
 def view_expenses():
     if not expenses:
-        print("No expenses found.")
+        print("\nNo expenses found.")
         return
-    
-    print("All expenses")
-    print("{:<5} {:<12} {:<15} {:<10} {:<20}".format("ID", "Date", "Category", "Amount", "Description"))
-    print("-" * 65)
+
+    print("\nAll Expenses")
+    print("{:<5} {:<12} {:<15} {:<12} {:<20}".format(
+        "ID", "Date", "Category", "Amount", "Description"))
+    print("-" * 70)
 
     for expense in expenses:
-        formatted_date = expense.date.strftime("%Y-%m-%d")
-        print("{:<5} {:<12} {:<15} {:<10} {:<20}".format(
+        print("{:<5} {:<12} {:<15} {:<12} {:<20}".format(
             expense.expense_id,
-            formatted_date,
+            expense.date.strftime("%Y-%m-%d"),
             expense.category,
             f"Rs.{expense.amount:.2f}",
             expense.description
         ))
 
+
+# ================= DELETE EXPENSE =================
 def reassign_ids():
     for index, expense in enumerate(expenses):
         expense.expense_id = index + 1
+
 
 def delete_expense():
     global expense_counter
 
     if not expenses:
-        print("No expenses to delete.")
+        print("\nNo expenses to delete.")
         return
-    
+
     try:
         expense_id = int(input("Enter Expense ID to delete: "))
     except ValueError:
@@ -139,17 +158,20 @@ def delete_expense():
             expenses.remove(expense)
             reassign_ids()
             expense_counter = len(expenses) + 1
+            save_expenses()
             print("Expense deleted successfully!")
             return
 
-    print("Expense not found!")    
+    print("Expense not found!")
 
+
+# ================= MONTHLY SUMMARY =================
 def monthly_summary():
     if not expenses:
-        print("No expenses available.")
+        print("\nNo expenses available.")
         return
 
-    month_input = input ("Enter month (YYYY-MM): ")
+    month_input = input("Enter month (YYYY-MM): ")
 
     try:
         selected_month = datetime.strptime(month_input, "%Y-%m")
@@ -161,22 +183,21 @@ def monthly_summary():
     category_totals = {}
 
     for expense in expenses:
-        if (
-            expense.date.year == selected_month.year and 
-            expense.date.month == selected_month.month
-        ):
+        if (expense.date.year == selected_month.year and
+                expense.date.month == selected_month.month):
             total += expense.amount
             category_totals[expense.category] = (
                 category_totals.get(expense.category, 0) + expense.amount
             )
-    
+
     if total == 0:
         print("No expenses found for this month.")
         return
-    
-    print(f"Summary for {month_input}")
-    print("-" * 30)
+
+    print(f"\nSummary for {month_input}")
+    print("-" * 40)
     print(f"Total Expense: Rs.{total:.2f}\n")
+
     print("{:<15} {:<12} {:<12}".format("Category", "Amount", "Percentage"))
     print("-" * 45)
 
@@ -186,32 +207,92 @@ def monthly_summary():
             category, amount, percentage
         ))
 
+
+# ================= SORTING =================
 def sort_expenses_by_date(order="asc"):
     if not expenses:
-        print("\n No expenses to sort.")
+        print("\nNo expenses to sort.")
         return
 
     reverse_order = True if order == "desc" else False
-
     expenses.sort(key=lambda expense: expense.date, reverse=reverse_order)
 
-    print("\n Expenses sorted successfully")
+    print("\nExpenses sorted successfully!")
     view_expenses()
 
+# ================= SEARCH EXPENSES ================
+def search_expenses():
+    if not expenses:
+        print("\nNo expenses available to search.")
+        return
+    
+    print("\nSearch By:")
+    print("1. Category")
+    print("2. Date (YYYY-MM-DD)")
+    print("3. Description Keyword")
+
+    choice = input("Enter your choice: ")
+
+    results = []
+
+    if choice == "1":
+        category_input = input("Enter category: ").strip().title()
+        results = [expense for expense in expenses
+                    if expense.category == category_input]
+    
+    elif choice == "2":
+        date_input = input("Enter date (YYYY-MM-DD): ").strip()
+        try:
+            search_date = datetime.strptime(date_input, "%Y-%m-%d")
+            results = [expense for expense in expenses
+                        if expense.date == search_date]
+        except ValueError:
+            print("Invalid date format.")
+            return
+
+    elif choice == "3":
+        keyword = input("Enter keyword: ").strip().lower()
+        results = [expense for expense in expenses
+                    if keyword in expense.description.lower()]
+    
+    else:
+        print("Invalid choice.")
+        return
+
+    if not results:
+        print("No results found.")
+        return
+
+    print("\nSearch Results")
+    print("{:<5} {:<12} {:<15} {:<12} {:<20}".format(
+        "ID", "Date", "Category", "Amount", "Description"))
+    print("-" * 70)
+
+    for expense in results:
+        print("{:<5} {:<12} {:<15} {:<12} {:<20}".format(
+            expense.expense_id,
+            expense.date.strftime("%Y-%m-%d"),
+            expense.category,
+            f"Rs.{expense.amount:.2f}",
+            expense.description
+        ))                   
+
+# ================= MAIN MENU =================
 def main_menu():
     load_expenses()
 
     while True:
-        print("EXPENSE TRACKER")
+        print("\nEXPENSE TRACKER")
         print("1. Add Expense")
         print("2. View Expenses")
         print("3. Delete Expense")
         print("4. Monthly Summary")
-        print("5. Sort in ascending order")
-        print("6. Sort in descending order")
-        print("7. Exit")
+        print("5. Sort Ascending")
+        print("6. Sort Descending")
+        print("7. Search Expenses")
+        print("8. Exit")
 
-        choice = input ("Enter your choice: ")
+        choice = input("Enter your choice: ")
 
         if choice == "1":
             add_expense()
@@ -226,10 +307,13 @@ def main_menu():
         elif choice == "6":
             sort_expenses_by_date("desc")
         elif choice == "7":
+            search_expenses()
+        elif choice == "8":
             print("Exiting...")
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main_menu()
